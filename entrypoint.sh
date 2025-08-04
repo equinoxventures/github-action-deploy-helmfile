eed7bcd2f7983abfd15a82e653708774562fa64b
Amrut Phadke <amrut546@gmail.com>
2025-08-03 23:53:34 -0500

    update dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,68 +48,69 @@ mv helmfile /usr/local/bin/helmfile
 rm helmfile.tar.gz
 
 echo "Helmfile installed at: /usr/local/bin/helmfile"
+exec /bin/bash
 # Used for debugging
-aws ${AWS_ENDPOINT_OVERRIDE:+--endpoint-url $AWS_ENDPOINT_OVERRIDE} sts --region ${AWS_REGION} get-caller-identity
+# aws ${AWS_ENDPOINT_OVERRIDE:+--endpoint-url $AWS_ENDPOINT_OVERRIDE} sts --region ${AWS_REGION} get-caller-identity
 
-# Login to Kubernetes Cluster.
-aws ${AWS_ENDPOINT_OVERRIDE:+--endpoint-url $AWS_ENDPOINT_OVERRIDE} eks --region ${AWS_REGION} update-kubeconfig --name ${CLUSTER_NAME}
+# # Login to Kubernetes Cluster.
+# aws ${AWS_ENDPOINT_OVERRIDE:+--endpoint-url $AWS_ENDPOINT_OVERRIDE} eks --region ${AWS_REGION} update-kubeconfig --name ${CLUSTER_NAME}
 
-# Read platform specific configs/info
-chamber export platform/${CLUSTER_NAME}/${ENVIRONMENT} --format yaml | yq --exit-status --no-colors  eval '{"platform": .}' - > /tmp/platform.yaml
+# # Read platform specific configs/info
+# chamber export platform/${CLUSTER_NAME}/${ENVIRONMENT} --format yaml | yq --exit-status --no-colors  eval '{"platform": .}' - > /tmp/platform.yaml
 
-APPLICATION_HELMFILE=$(pwd)/${HELMFILE_PATH}/${HELMFILE}
+# APPLICATION_HELMFILE=$(pwd)/${HELMFILE_PATH}/${HELMFILE}
 
-BASIC_ARGS="--environment ${ENVIRONMENT} --file ${APPLICATION_HELMFILE} --state-values-file /tmp/platform.yaml"
-EXTRA_VALUES_ARGS=""
-DEBUG_ARGS=""
+# BASIC_ARGS="--environment ${ENVIRONMENT} --file ${APPLICATION_HELMFILE} --state-values-file /tmp/platform.yaml"
+# EXTRA_VALUES_ARGS=""
+# DEBUG_ARGS=""
 
-if [[ "${HELM_DEBUG}" == "true" ]]; then
-	DEBUG_ARGS=" --debug"
-fi
+# if [[ "${HELM_DEBUG}" == "true" ]]; then
+# 	DEBUG_ARGS=" --debug"
+# fi
 
-if [[ -n "$HELM_VALUES_YAML" ]]; then
-  echo -e "Using extra values:\n${HELM_VALUES_YAML}"
-  HELM_VALUES_FILE="/tmp/extra_helm_values.yml"
-  echo "$HELM_VALUES_YAML" > "$HELM_VALUES_FILE"
-  EXTRA_VALUES_ARGS="--state-values-file /tmp/extra_helm_values.yml"
-fi
+# if [[ -n "$HELM_VALUES_YAML" ]]; then
+#   echo -e "Using extra values:\n${HELM_VALUES_YAML}"
+#   HELM_VALUES_FILE="/tmp/extra_helm_values.yml"
+#   echo "$HELM_VALUES_YAML" > "$HELM_VALUES_FILE"
+#   EXTRA_VALUES_ARGS="--state-values-file /tmp/extra_helm_values.yml"
+# fi
 
-if [[ "${OPERATION}" == "deploy" ]]; then
-	OPERATION_COMMAND="helmfile ${BASIC_ARGS} ${EXTRA_VALUES_ARGS} ${DEBUG_ARGS} apply"
-	echo "Executing: ${OPERATION_COMMAND}"
-	${OPERATION_COMMAND}
+# if [[ "${OPERATION}" == "deploy" ]]; then
+# 	OPERATION_COMMAND="helmfile ${BASIC_ARGS} ${EXTRA_VALUES_ARGS} ${DEBUG_ARGS} apply"
+# 	echo "Executing: ${OPERATION_COMMAND}"
+# 	${OPERATION_COMMAND}
 
-	RELEASES=$(helmfile ${BASIC_ARGS} ${EXTRA_VALUES_ARGS} ${DEBUG_ARGS} list --output json | jq .[].name -r)
-  for RELEASE in ${RELEASES}
-  do
-    echo "Processing release: ${RELEASE}"
-    echo "Executing kubectl command: kubectl --namespace ${NAMESPACE} get -l ${RELEASE_LABEL_NAME}=${RELEASE} ${URL_RESOURCE_TYPE} -o json"
-    ENTRYPOINT=$(kubectl --namespace ${NAMESPACE} get -l ${RELEASE_LABEL_NAME}=${RELEASE} ${URL_RESOURCE_TYPE} -o json | jq --raw-output '[.items[].metadata.annotations["outputs.webapp-url"]] | first')
-    if [[ "${ENTRYPOINT}" != "" ]]; then
-      echo "Found webapp-url for release ${RELEASE}: ${ENTRYPOINT}"
-      echo "webapp-url=${ENTRYPOINT}" >> $GITHUB_OUTPUT
-    else
-      echo "No webapp-url found for release ${RELEASE}"
-    fi
-  done
+# 	RELEASES=$(helmfile ${BASIC_ARGS} ${EXTRA_VALUES_ARGS} ${DEBUG_ARGS} list --output json | jq .[].name -r)
+#   for RELEASE in ${RELEASES}
+#   do
+#     echo "Processing release: ${RELEASE}"
+#     echo "Executing kubectl command: kubectl --namespace ${NAMESPACE} get -l ${RELEASE_LABEL_NAME}=${RELEASE} ${URL_RESOURCE_TYPE} -o json"
+#     ENTRYPOINT=$(kubectl --namespace ${NAMESPACE} get -l ${RELEASE_LABEL_NAME}=${RELEASE} ${URL_RESOURCE_TYPE} -o json | jq --raw-output '[.items[].metadata.annotations["outputs.webapp-url"]] | first')
+#     if [[ "${ENTRYPOINT}" != "" ]]; then
+#       echo "Found webapp-url for release ${RELEASE}: ${ENTRYPOINT}"
+#       echo "webapp-url=${ENTRYPOINT}" >> $GITHUB_OUTPUT
+#     else
+#       echo "No webapp-url found for release ${RELEASE}"
+#     fi
+#   done
 
 
-elif [[ "${OPERATION}" == "destroy" ]]; then
+# elif [[ "${OPERATION}" == "destroy" ]]; then
 
-	set +e
-	kubectl get ns ${NAMESPACE}
-	NAMESPACE_EXISTS=$?
-	set -e
+# 	set +e
+# 	kubectl get ns ${NAMESPACE}
+# 	NAMESPACE_EXISTS=$?
+# 	set -e
 
-	if [[ ${NAMESPACE_EXISTS} -eq 0  ]]; then
-		OPERATION_COMMAND="helmfile ${BASIC_ARGS} ${EXTRA_VALUES_ARGS} ${DEBUG_ARGS} destroy"
-		echo "Executing: ${OPERATION_COMMAND}"
-		${OPERATION_COMMAND}
+# 	if [[ ${NAMESPACE_EXISTS} -eq 0  ]]; then
+# 		OPERATION_COMMAND="helmfile ${BASIC_ARGS} ${EXTRA_VALUES_ARGS} ${DEBUG_ARGS} destroy"
+# 		echo "Executing: ${OPERATION_COMMAND}"
+# 		${OPERATION_COMMAND}
 
-		RELEASES_COUNTS=$(helm --namespace ${NAMESPACE} list --output json | jq 'length')
+# 		RELEASES_COUNTS=$(helm --namespace ${NAMESPACE} list --output json | jq 'length')
 
-    if [[ "${RELEASES_COUNTS}" == "0" ]]; then
-    	kubectl delete ns ${NAMESPACE}
-    fi
-  fi
-fi
+#     if [[ "${RELEASES_COUNTS}" == "0" ]]; then
+#     	kubectl delete ns ${NAMESPACE}
+#     fi
+#   fi
+# fi
